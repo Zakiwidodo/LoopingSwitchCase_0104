88689b6bb50da03ca9920a2eeb0fb435ea279812
Zakiwidodo <muhzakiwidodo@gmail.com>
2025-03-18 15:32:17 +0700

    membuat program perulangan

---
+++ file1.cpp
@@ -0,0 +1,23 @@
+#include  <iostream>
+using namespace std;
+
+int main(){
+    //perulangan for untuk pencacah naik
+    int i;
+    cout << "perulangan for pencacah Naik" << endl;
+    for (i = 0; 1 <= 4; i++){
+        cout << "nilai i :"<< i
+        << "Lari Keliling lapangan" << endl;
+}
+
+cout << "Nilai 1 terakhir : " << i << endl;
+cout << endl;
+
+//perulangan for untuk pencacah turun
+cout << "perulangan for pencacah turun" << endl;
+for (i = 4; i >= 0; i--){
+        cout << "Nilai i : " << i
+        << "Lari Keliling lapangan" << endl;
+}
+
+}
